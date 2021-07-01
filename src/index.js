6f52f658b6fcd9d3a2618f7f7e4b8c45523f191f
AndyGuit <totalslash@msn.com>
2021-07-01 15:25:44 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (arguments.length === 0 || array.length === 0) { return 0; }
+  const result = array.sort((a, b) => a - b);
+  return result[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (arguments.length === 0 || array.length === 0) { return 0; }
+  const result = array.sort((a, b) => a - b);
+  return result[result.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (arguments.length === 0 || array.length === 0) { return 0; }
+  return (array.reduce((sum, current) => sum + current, 0)) / array.length;
 }
